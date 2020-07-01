55c5dcf52eb75d1e976f5bbe6417da8e76733a4c
Crystal Butler <crystal.butler@arup.com>
2020-07-01 19:25:51 -0400

    edit args help for visualize_distribution

--- visualize_distribution.py
+++ visualize_distribution.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('scores_dir', help='full path to a directory containing all pairs synonymy scores', type=str)
-parser.add_argument('histogram_dir', help='full path to a directory where the histogram plot will be written', type=str)
+parser.add_argument('scores_dir', help='path to a directory containing all pairs synonymy scores', type=str)
+parser.add_argument('histogram_dir', help='path to a directory where the histogram plot will be written', type=str)
 parser.add_argument('bin_count', help='the number of bins used in the histogram', type=int)
 args = parser.parse_args()
 
