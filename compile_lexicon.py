f1375c61c21ac4e83db1a9b71128f271d85ef0c1
Crystal Butler <crystal.butler@arup.com>
2020-08-29 21:19:46 -0400

    eliminate unneeded placeholder functions

--- compile_lexicon.py
+++ compile_lexicon.py
@@ -115,93 +115,30 @@ def print_image_record(image_record):
     print('\n')
 
 
-def make_arrays(scores_path, labels_path):
-    """Read scores and labels in from files. Convert them to ndarrays for clustering.
-    Transform similarity (proximity) scores to distances."""
-    pairs_scores = pd.read_csv(scores_path, header=None)
-    labels = pd.read_csv(labels_path, header=None)
-    scores_array = np.array(pairs_scores[0][:])
-    scores_norm = normalize_array(scores_array)
-    distances_array = 1 - scores_norm
-    labels_array = np.array(labels[0][:])
-    assert len(pairs_scores[0]) == len(distances_array), "Scores dataframe and distances array should be the same length."
-    assert len(labels[0]) == len(labels_array), "Labels dataframe and labels array should be the same length."
-    return distances_array, labels_array
+# def format_cluster_stats(cophenetic_coefficient, cluster_membership, pct):
+#     """Pretty print layout for clustering statistics; can be appended to the dendrogram or saved out as a file."""
+#     stats_printout = '---------------------------------------------------------------------------------\n'
+#     stats_printout += 'Agglomerative Hierarchical Clustering Statistics\n---------------------------------------------------------------------------------\n'
+#     stats_printout += ('Cophenectic correlation coefficient: ' + str(cophenetic_coefficient) + '\n')
+#     stats_printout += ('Cluster: Count\n')
+#     for key in cluster_membership.keys():
+#         stats_printout += (str(key) + ': ' + str(cluster_membership[key]) + '\n')
+#     cluster_max_membership = max(cluster_membership.items(), key=lambda x : x[1])
+#     stats_printout += ('Cluster ' + str(cluster_max_membership[0]) + ' with ' + str(cluster_max_membership[1]) + ' members has ' + str(pct) + '% of the membership.\n')
+#     pass_fail = classify_pass_fail(pct)
+#     stats_printout += ('Cluster coherence test: ' + pass_fail)
+#     return stats_printout
 
 
-def normalize_array(scores_array):
-    scores_norm = (scores_array - np.min(scores_array))/np.ptp(scores_array)
-    scores_norm = scores_norm.round(decimals=6)  # clean up floating point errors and reduce significant digits
-    return scores_norm
-
-
-def check_expected_distances_count(labels_array):
-    """The distances array should be a serialized upper triangular label x label matrix,
-       with entries below the diagonal omitted."""
-    expected_distances_count = int((len(labels_array) * (len(labels_array) - 1)) / 2)
-    return expected_distances_count
-
-
-def build_linkage_matrix(distances_array):
-    """Create the linkage matrix Z (perform hierarchical/agglomerative clustering)."""
-    linkage_matrix = sch.linkage(distances_array, 'average')
-    # Fix distances that have become less than 0 due to floating point errors.
-    for i in range(len(linkage_matrix)):
-        if linkage_matrix[i][2] < 0:
-            linkage_matrix[i][2] = 0
-    return linkage_matrix
-
-
-def calculate_cluster_stats(linkage_matrix, distances_array):
-    """Calculate clustering statistics for cophenetic coefficient correlation, 
-    the number of clusters, the count of labels per cluster and the 
-    percent membershp in the largest cluster."""
-    clusters = sch.fcluster(linkage_matrix, args.dendro_cutoff, criterion='distance')
-    cluster_enumeration = np.unique(clusters)
-    # Calculate the cophenetic correlation coefficient statistic: closer to 1 is better.
-    cophenetic_coefficient, _ = sch.cophenet(linkage_matrix, distances_array)
-    # Get membership counts for each cluster.
-    cluster_membership = {}
-    for value in cluster_enumeration:
-        member_count = np.count_nonzero(clusters == value)
-        cluster_membership[value] = member_count
-    # Calculate the percentage membership in the largest cluster.
-    c_max = max(cluster_membership.values())
-    c_sum = sum(cluster_membership.values())
-    pct = 100 * (c_max / c_sum)
-    return cophenetic_coefficient, cluster_membership, pct
-
-
-def format_cluster_stats(cophenetic_coefficient, cluster_membership, pct):
-    """Pretty print layout for clustering statistics; can be appended to the dendrogram or saved out as a file."""
-    stats_printout = '---------------------------------------------------------------------------------\n'
-    stats_printout += 'Agglomerative Hierarchical Clustering Statistics\n---------------------------------------------------------------------------------\n'
-    stats_printout += ('Cophenectic correlation coefficient: ' + str(cophenetic_coefficient) + '\n')
-    stats_printout += ('Cluster: Count\n')
-    for key in cluster_membership.keys():
-        stats_printout += (str(key) + ': ' + str(cluster_membership[key]) + '\n')
-    cluster_max_membership = max(cluster_membership.items(), key=lambda x : x[1])
-    stats_printout += ('Cluster ' + str(cluster_max_membership[0]) + ' with ' + str(cluster_max_membership[1]) + ' members has ' + str(pct) + '% of the membership.\n')
-    pass_fail = classify_pass_fail(pct)
-    stats_printout += ('Cluster coherence test: ' + pass_fail)
-    return stats_printout
-
-
-def classify_pass_fail(pct):
-    """The clustering coherence test is based on membership percentage in the largest cluster."""
-    pass_fail = 'pass' if pct >= 75 else 'fail'
-    return pass_fail
-
-
-def make_output_filenames(pct, dendro_name):
-    """Write statistics and dendrograms to Pass or Fail directories based on the clustering coherence test."""
-    if pct >= 75:
-        dendro_file = os.path.join(args.clustering_dir, 'Dendrograms/Pass/' + dendro_name + '.png')
-        stats_file = os.path.join(args.clustering_dir, 'Statistics/Pass/' + dendro_name + '.txt')
-    else:
-        dendro_file = os.path.join(args.clustering_dir, 'Dendrograms/Fail/' + dendro_name + '.png')
-        stats_file = os.path.join(args.clustering_dir, 'Statistics/Fail/' + dendro_name + '.txt')
-    return dendro_file, stats_file
+# def make_output_filenames(pct, dendro_name):
+#     """Write statistics and dendrograms to Pass or Fail directories based on the clustering coherence test."""
+#     if pct >= 75:
+#         dendro_file = os.path.join(args.clustering_dir, 'Dendrograms/Pass/' + dendro_name + '.png')
+#         stats_file = os.path.join(args.clustering_dir, 'Statistics/Pass/' + dendro_name + '.txt')
+#     else:
+#         dendro_file = os.path.join(args.clustering_dir, 'Dendrograms/Fail/' + dendro_name + '.png')
+#         stats_file = os.path.join(args.clustering_dir, 'Statistics/Fail/' + dendro_name + '.txt')
+#     return dendro_file, stats_file
 
 
 if __name__ == '__main__':
